22b7f1eb4920d9c618fa35d656c33bf078266f9e
DoDoBest <send1684@gmail.com>
2024-04-16 16:02:05 +0900

    init: 초기 프로젝트 생성

---
+++ app/src/main/java/com/dothebestmayb/nbc_challenge_kakaoapi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dothebestmayb.nbc_challenge_kakaoapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

53dbc86b3c8bb74d4306686acfaad685d5ce129e
DoDoBest <send1684@gmail.com>
2024-04-18 19:46:30 +0900

    build: retrofit, moshi 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,10 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.viewpager2:viewpager2:1.1.0-beta02")
 
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

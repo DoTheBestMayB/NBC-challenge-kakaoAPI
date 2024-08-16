317aebe8273845dde3c18c4d35803aff5428f6f6
DoDoBest <send1684@gmail.com>
2024-08-16 16:03:31 +0900

    feat: edgeToEdge 적용

---
+++ app/src/main/java/com/dothebestmayb/nbc_challenge_kakaoapi/presenter/ui/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.dothebestmayb.nbc_challenge_kakaoapi.presenter.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dothebestmayb.nbc_challenge_kakaoapi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adjustInsets()
+    }
+
+    private fun adjustInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

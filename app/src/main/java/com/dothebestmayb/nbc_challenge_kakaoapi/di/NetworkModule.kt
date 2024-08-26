6bfef8eb65a16fdfc153449d2c5edebdc582a77a
DoDoBest <send1684@gmail.com>
2024-08-26 23:33:17 +0900

    feat: NetworkModule 설정

---
+++ app/src/main/java/com/dothebestmayb/nbc_challenge_kakaoapi/di/NetworkModule.kt
@@ -0,0 +1,64 @@
+package com.dothebestmayb.nbc_challenge_kakaoapi.di
+
+import com.dothebestmayb.nbc_challenge_kakaoapi.config.Logging
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMoshiConverter(moshi: Moshi): MoshiConverterFactory {
+        return MoshiConverterFactory.create(moshi)
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = Logging.HTTP_DEBUG_LEVEL
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideKakaoRetrofit(
+        okHttpClient: OkHttpClient,
+        moshiConverterFactory: MoshiConverterFactory,
+    ): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(KAKAO_BASE_URL)
+            .addConverterFactory(moshiConverterFactory)
+            .build()
+    }
+
+    companion object {
+        private const val KAKAO_BASE_URL = "https://dapi.kakao.com/v2/"
+    }
+}
---
+++ app/src/release/java/com/dothebestmayb/nbc_challenge_kakaoapi/config/Logging.kt
@@ -0,0 +1,7 @@
+package com.dothebestmayb.nbc_challenge_kakaoapi.config
+
+import okhttp3.logging.HttpLoggingInterceptor
+
+object Logging {
+    val HTTP_DEBUG_LEVEL = HttpLoggingInterceptor.Level.NONE
+}

535ea2bbdd3908c3e2886449f41a2700798524a8
DoDoBest <send1684@gmail.com>
2024-08-26 23:25:39 +0900

    build: buildTypes를 kts 문법으로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,12 +22,17 @@ android {
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+
+        getByName("debug") {
+            applicationIdSuffix = ".debug"
+            isDebuggable = true
         }
     }
     compileOptions {

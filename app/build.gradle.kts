4755481df15a5015371b5ea64f181033a358f393
DoDoBest <send1684@gmail.com>
2024-08-29 10:31:12 +0900

    fix: moshi generatorAdapter를 이용하기 위한 codegen 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,7 @@ dependencies {
     // Moshi
     implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
     implementation("com.squareup.moshi:moshi:1.15.1")
+    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
 
     // coil
     implementation("io.coil-kt:coil:2.7.0")

2fcab31998fb20774af8a56a5cd6816ca0f459dd
DoDoBest <send1684@gmail.com>
2024-08-29 12:42:28 +0900

    fix: API response와 일치하지 않는 파라미터 이름 앞에 Json 애노테이션으로 이름 명시

--- app/src/main/java/com/dothebestmayb/nbc_challenge_kakaoapi/data/remote/model/response/Document.kt
+++ app/src/main/java/com/dothebestmayb/nbc_challenge_kakaoapi/data/remote/model/response/Document.kt
@@ -1,14 +1,15 @@
 package com.dothebestmayb.nbc_challenge_kakaoapi.data.remote.model.response
 
+import com.squareup.moshi.Json
 import java.time.LocalDateTime
 
 data class Document(
     val collection: String,
-    val thumbnailUrl: String,
-    val imageUrl: String,
+    @Json(name = "thumbnail_url") val thumbnailUrl: String,
+    @Json(name = "image_url") val imageUrl: String,
     val width: Int,
     val height: Int,
-    val displaySiteName: String,
-    val docUrl: String,
+    @Json(name = "display_sitename") val displaySiteName: String,
+    @Json(name = "doc_url") val docUrl: String,
     val datetime: LocalDateTime,
 )
--- app/src/main/java/com/dothebestmayb/nbc_challenge_kakaoapi/data/remote/model/response/Meta.kt
+++ app/src/main/java/com/dothebestmayb/nbc_challenge_kakaoapi/data/remote/model/response/Meta.kt
@@ -1,10 +1,11 @@
 package com.dothebestmayb.nbc_challenge_kakaoapi.data.remote.model.response
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class Meta(
-    val totalCount: Int,
-    val pageableCount: Int,
-    val isEnd: Boolean,
+    @Json(name = "total_count") val totalCount: Int,
+    @Json(name = "pageable_count") val pageableCount: Int,
+    @Json(name = "is_end") val isEnd: Boolean,
 )
